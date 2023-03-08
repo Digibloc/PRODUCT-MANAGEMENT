cd9c2adca5ca5709e4362420b089fff689ff6f1a
Alvin Kiveu <67068458+alvin-kiveu@users.noreply.github.com>
2023-03-08 12:58:25 +0300

    adding all data

---
+++ header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PRODUCT MANGE</title>
+    <!-- BOOSTRAP CDN -->
+    <link rel="stylesheet" href="assets/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/dist/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/dist/css/bootstrap-grid.css">
+    <link rel="stylesheet" href="assets/dist/css/bootstrap-grid.min.css">
+    <link rel="stylesheet" href="assets/dist/css/bootstrap-reboot.css">
+    <link rel="stylesheet" href="assets/dist/css/bootstrap-reboot.min.css">
+    <link rel="stylesheet" href="assets/dist/css/bootstrap-utilities.css">
+    <link rel="stylesheet" href="assets/dist/css/bootstrap-utilities.min.css">
+    <link rel="stylesheet" href="scss/main.css">
+</head>
+
+<body>
+<div class="container-fluid bg-primary">
+        <div class="row">
+            <div class="col-12">
+                <h1 class="text-center text-light">PRODUCT MANAGEMENT</h1>
+            </div>
+        </div>
+    </div>
---
+++ list.php
@@ -0,0 +1,34 @@
+<?php include "header.php"; ?>
+<div class="container bg-light h-100">
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col bg-primary">#</th>
+      <th scope="col bg-primary">First</th>
+      <th scope="col bg-primary">Last</th>
+      <th scope="col bg-primary">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+</div>
+<?php include "footer.php"; ?>
