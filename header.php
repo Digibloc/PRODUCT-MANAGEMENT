1668a4047b52c548c3769aa665748afe64180aa8
Alvin Kiveu <67068458+alvin-kiveu@users.noreply.github.com>
2023-03-08 13:04:37 +0300

    adding all new data

--- header.php
+++ header.php
@@ -26,3 +26,13 @@
             </div>
         </div>
     </div>
+    <div class="container bg-light h-100">
+    <div class="row justify-content-center">
+        <a href="add.php" class="col md-3 text-center text-light  bg-success text-decoration-none">
+            Add Product
+        </a>
+        <a href="list.php" class="col md-3 text-center bg-secondary text-light text-decoration-none">
+            List Product
+        </a>
+    </div>
+</div>
--- index.php
+++ index.php
@@ -1,12 +1,3 @@
 <?php include "header.php"; ?>
-<div class="container bg-light h-100">
-    <div class="row justify-content-center">
-        <a href="add.php" class="col md-3 text-center text-light  bg-success text-decoration-none">
-            Add Product
-        </a>
-        <a href="list.php" class="col md-3 text-center bg-secondary text-light text-decoration-none">
-            List Product
-        </a>
-    </div>
-</div>
+
 <?php include "footer.php"; ?>
